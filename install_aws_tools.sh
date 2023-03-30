fb0a8f12cd6432c02ab4bfda370875304d52aa69
ralph-malph <rc6152487@gmail.com>
2023-03-30 11:39:00 +0100

    more install scripts

---
+++ install_aws_tools.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+./awscli-bundle/install -b ~/bin/aws
+./awscli-bundle/install -h
---
+++ install_s3cmd.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ~
+git clone https://github.com/s3tools/s3cmd.git
+cd s3cmd
+sudo python setup.py install
